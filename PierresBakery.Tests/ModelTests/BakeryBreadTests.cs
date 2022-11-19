19913e31150e7838dcfa53c06e5a1697a66d1ca5
Grace Lee <gracexsnoopy@gmail.com>
2022-11-18 20:55:37 -0800

    Pass first test for bread class

--- PierresBakery.Tests/ModelTests/BakeryBreadTests.cs
+++ PierresBakery.Tests/ModelTests/BakeryBreadTests.cs
@@ -6,6 +6,11 @@ namespace BakeryBread.Models.Tests
   [TestClass]
   public class Bread
   {
-
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfItem_Bread() 
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
   }
 }
