fd3d16180631e0e9bfa23b6215562b7337a607ec
Grace Lee <gracexsnoopy@gmail.com>
2022-11-18 20:47:15 -0800

    Add extra file for another class and extra test file for it

---
+++ PierresBakery.Tests/ModelTests/BakeryBreadTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BakeryBread.Models;
+
+namespace BakeryBread.Models.Tests 
+{
+  [TestClass]
+  public class Bread
+  {
+
+  }
+}
---
+++ PierresBakery.Tests/ModelTests/BakeryPastryTests.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BakeryPastry.Models;
+
+namespace BakeryPastry.Models.Tests 
+{
+  [TestClass]
+  public class Pastry
+  {
+  }
+}
