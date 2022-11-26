a73d1bf999747de9ba6976da4a4e25a90a2b955e
Grace Lee <gracexsnoopy@gmail.com>
2022-11-26 14:16:55 -0800

    Change so that user knows about buy 2, get 1 free deal

--- PierresBakery/PierresBakery.cs
+++ PierresBakery/PierresBakery.cs
@@ -7,7 +7,7 @@ class Program
   {
     
     Console.WriteLine("Welcome to Pierre's Bakery!");
-    Console.WriteLine("A single loaf of bread costs $5. Every 3rd loaf of bread is free!");
+    Console.WriteLine("A single loaf of bread costs $5. But buy 2, get 1 loaf free! Every 3rd loaf of bread is free!");
     Console.WriteLine("One pastry is $2. Four pastries cost $7, five pastries cost $9, and six pastries cost $10. Dicounts apply respectively if you would like more than 6 pastries.");
     Console.WriteLine("Enter the amount of bread loaves you would like.");
     string breadAmountInput = Console.ReadLine(); 
@@ -21,6 +21,9 @@ class Program
     newPastry.TotalPastryCost();
     int finalPastryCost = newPastry.TotalPastryCost();
     double totalOrderCost = finalBreadCost + finalPastryCost;
+    double totalFreeBread = Math.Floor(newBread.BreadAmount / 3);
+    Console.WriteLine("You ordered " + newBread.BreadAmount + " bread loaves and " + newPastry.PastryAmount + " pastries.");
+    Console.WriteLine(totalFreeBread + " loaf/loaves of your bread were for free!");
     Console.WriteLine("Your total is $" + totalOrderCost + ".");
     Console.WriteLine("Thank you for shopping at Pierre's Bakery!");
     Console.WriteLine("Have a good day!");
